b3d418812bd21c47ad776f3e8133832386456b6a
ahmed <ahmed.nageeb.mahmoud.hssan@gmail.com>
2023-02-23 22:08:47 +0200

    Clean Employee Controller

--- LearnNetCoreWepAPI/Controllers/EmployeeController.cs
+++ LearnNetCoreWepAPI/Controllers/EmployeeController.cs
@@ -11,33 +11,5 @@ namespace LearnNetCoreWepAPI.Controllers
     [Route("[controller]")]
     public class EmployeeController : ControllerBase
     {
-        public IRepository<Employee> _Entity;
-        public EmployeeController(IRepository<Employee> entity)
-        {
-            db = entity;
-        }
-
-        [HttpPost(Name ="Add")]
-        public async Task<string> AddEmployee([FromBody] EployeeDTO emp)
-        {
-            return false?"Addedd Successfully":"Some Error Has Been";
-        
-        }
-
-        [HttpGet]
-        public async Task<Employee> FindEmployee()
-        {
-            db.Users.Find(who); 
-            db.Entry(user)
-                    .Collection(u => u.Connections)
-                    .Query()
-                    .Where(c => c.Connected == true)
-                    .Load();
-
-
-            db._entities.MinBy(c => c.Age);
-
-            return null;
-        }
     }
 }
